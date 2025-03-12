665216462758b423b7ce500e380c0fddf54df32a
Dmitriy <95078422+Amid3222@users.noreply.github.com>
2025-03-12 23:39:44 +0300

    15к edit parametr

--- src/main/java/com/walking/excels/Main.java
+++ src/main/java/com/walking/excels/Main.java
@@ -6,7 +6,7 @@ public class Main {
   public static void main(String[] args) throws IOException {
     ExcelService excelService = new ExcelService();
     RandStorage randStorage = new RandStorage();
-    for (int i = 0; i < 150_000; i++) randStorage.putToStorageNameAndCounts();
+    for (int i = 0; i < 15_000; i++) randStorage.putToStorageNameAndCounts();
 
     randStorage.sortDecrementMap();
 
