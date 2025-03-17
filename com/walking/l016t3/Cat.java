d176b2110fce09a304258778a20d1a47d6353fe2
Dmitriy <95078422+Amid3222@users.noreply.github.com>
2025-03-17 11:10:27 +0300

    commit

---
+++ com/walking/l016t3/Cat.java
@@ -0,0 +1,8 @@
+package com.walking.l016t3;
+
+public class Cat implements Animal{
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
---
+++ com/walking/l016t3/Cow.java
@@ -0,0 +1,9 @@
+package com.walking.l016t3;
+
+public class Cow implements Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Moo");
+    }
+}
