9b88777ab00f1457159fee7ceaa820202305aad6
Dmitriy <95078422+Amid3222@users.noreply.github.com>
2025-03-23 19:33:59 +0300

    commit

---
+++ com/walking/l20t2/Main.java
@@ -0,0 +1,34 @@
+package com.walking.l20t2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InputValidationException {
+        System.out.print("Выберите тип фигуры(треугольник/квадрат):");
+        RightShape figure;
+        Scanner scanner = new Scanner(System.in);
+        String figureType = scanner.nextLine();
+        if (figureType.equals("треугольник")) figure = new Triangle();
+        else if (figureType.equals("квадрат")) figure = new Square();
+        else throw new InputValidationException("Такая фигура не существует: ", figureType);
+        System.out.printf("\nВведите длинну стороны:");
+        figure.drawFigure(scanner.nextInt());
+        scanner.close();
+     /*   switch (scanner.nextLine()) {
+            case "треугольник":
+                System.out.printf("\nВведите длинну стороны:");
+                RightShape triangle = new Triangle();
+                triangle.drawFigure(scanner.nextInt());
+                triangle.getFigure();
+                break;
+            case "квадрат":
+                System.out.printf("\nВведите длинну стороны:");
+                RightShape square = new Square();
+                square.drawFigure(scanner.nextInt());
+                square.getFigure();
+                break;
+        }
+*/
+    }
+}
+
