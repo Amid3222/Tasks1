0bba0d6949032d4c066e60b808839bdf07a2091e
Dmitriy <95078422+Amid3222@users.noreply.github.com>
2025-03-24 00:29:16 +0300

    commit

---
+++ com/walking/l21t1/Car.java
@@ -0,0 +1,46 @@
+package com.walking.l21t1;
+
+public final class Car {
+    private final String name;
+    private final int year;
+    private final String number;
+    private final int hashValue;
+
+    public Car(String name, int year, String number) {
+        this.name = name;
+        this.year = year;
+        this.number = number;
+        this.hashValue = number.hashCode() + year * 31;
+    }
+
+    public Car(int year, String number) {
+        this.year = year;
+        this.name = "Unknown";
+        this.number = number;
+        this.hashValue = number.hashCode() + year * 31;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(hashValue);
+    }
+
+    //override toString
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return obj instanceof Car car && (this.number.equals(car.number) && this.year == car.year);
+    }
+}
